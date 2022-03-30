54b57d59be5e8a55918aa9268718f46a82f02927
Brittney Solorio <angelisolorio@gmail.com>
2022-03-30 15:44:09 -0500

    working with webscraping

--- webscraping-COVID.py
+++ webscraping-COVID.py
@@ -19,8 +19,75 @@ url = 'https://www.worldometers.info/coronavirus/country/us'
 # Request in case 404 Forbidden error
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
+req = Request(url, headers = headers)
+
+webpage = urlopen(req).read()
+
+soup = BeautifulSoup(webpage, 'html.parser')
+
+title = soup.title 
+
+print(title.text)
+
+table_rows = soup.findAll("tr")
+
+state_death_ratio = ""
+state_best_testing = ""
+state_worst_testing = ""
+highest_death_ratio = 0.0
+high_test_ratio = 0.0
+low_test_ratio = 100.0
+
+for row in table_rows[2:51]:
+    td = row.findAll("td")
+
+    state = td[1].text
+    total_cases = int(td[2].text.replace(',', ''))
+    total_deaths = int(td[4].text.replace(',', ''))
+    total_tested = int(td[10].text.replace(',', ''))
+    death_ratio = total_deaths/total_cases
+    test_ratio = total_cases/total_tested 
+
+    print(f"State:{state}")
+    print(f"Total Cases:{total_cases}")
+    print(f"Total Deaths:{total_deaths}")
+    print(f"Total Tested:{total_tested}")
+
+    if death_ratio > highest_death_ratio:
+        state_death_ratio = state 
+        highest_death_ratio = death_ratio
+    
+    if test_ratio > high_test_ratio:
+        state_worst_testing = state
+        high_test_ratio = test_ratio
+
+    if test_ratio < low_test_ratio:
+        state_best_testing = state
+        low_test_ratio = test_ratio 
+    
+    #Pennsylvania - highest death ratio
+    #Lowest testing ratio - Massachussetts 
+    #North Dakota - worst testing ratio 
+
+print("State wtih the highest death ratio is: ", state_death_ratio)
+print("Death Ratio:", format(highest_death_ratio, ".2%"))
+print()
+print()
+print("State wtih the highest test ratio is: ", state_best_testing)
+print("Test Ratio:", format(state_best_testing, ".2"))
+print()
+print()
+print("State wtih the lowest death ratio is: ", state_worst_testing)
+print("Test Ratio:", format(state_worst_testing, ".2"))
+print()
+print()
 
 
+input()
+
+#Highest death ratio 
+#Highest test ratio
+#Lowest test ratio 
 
 
 
